403ffd16f7865f4113d348589dd2624dd441a002
tjujingzong <470699397@qq.com>
2023-12-07 22:32:48 +0800

    fix some bugs

--- redis_mem.sh
+++ redis_mem.sh
@@ -6,11 +6,11 @@
 # io_levels=(0 1 2 3 4 5)
 
 # 初始化 memory_levels 数组
-memory_levels=()
+memory_levels=("4K")
 
 # 设置循环的起始值和上限（单位：MB）
-start=1
-limit=6145 # 6145
+start=256
+limit=7680 # 6144
 
 # 填充 memory_levels 数组
 for ((mem = start; mem <= limit; mem += 256)); do
@@ -20,9 +20,9 @@ done
 # Redis benchmark configuration
 host="172.17.0.2"
 port="6379"
-benchmark_duration="20s"
+benchmark_duration="20"
 monitor_duration="4" # 设置监控时长短于压力测试时长
-benchmark_cmd="redis-benchmark -h $host -p $port -t get -c 50 -n 100000"
+benchmark_cmd="redis-benchmark -h $host -p $port -t set -c 50 -n 20000 -d 128"
 
 # Get the PID of the Redis container
 redis_pid=$(pidof redis-server)
@@ -47,12 +47,12 @@ echo "Starting stress tests..."
 
 for mem in ${memory_levels[@]}; do
     # Apply stress in background
-    stress-ng --vm 1 --vm-bytes $mem --timeout $benchmark_duration
+    stress-ng --vm 2 --vm-bytes $mem --timeout $benchmark_duration &
 
-    sleep 4
+    sleep 6
     # Start Redis benchmark in background and save its output
-    start_time=$(date +%s) # 记录开始时间
     (
+        echo "start monitoring......"
         for i in {1..3}; do
             $benchmark_cmd | grep "requests per second" >>$benchmark_result_file
         done
@@ -62,18 +62,18 @@ for mem in ${memory_levels[@]}; do
     sudo perf stat -e cycles,instructions -x, -p $redis_pid -o temp1.txt sleep $monitor_duration &
 
     # Monitor system performance with perf
-    sudo perf stat -d -x, -a -o temp2.txt sleep $monitor_duration
+    sudo perf stat -d -x, -a -o temp2.txt sleep $monitor_duration &
 
     # Cleanup
     wait
     end_time=$(date +%s)                # 记录结束时间
     duration=$((end_time - start_time)) # 计算持续时间
-    echo "mem $mem $duration seconds" >>$benchmark_result_file
+    echo "mem $mem" >>$benchmark_result_file
     pkill stress-ng
     pkill redis-benchmark
     cat temp1.txt >>$redis_cpi
     cat temp2.txt >>$sys_result
-    sleep 1
+    sleep 3
 done
 echo "Stress tests completed."
 # End of experiments
--- test.sh
+++ test.sh
@@ -1,12 +1,11 @@
-host="172.17.0.2"
-port="6379"
-benchmark_cmd="redis-benchmark -h $host -p $port -t get -c 50 -n 100000"
-start_time=$(date +%s) # 记录开始时间
-(
-    for i in {1..3}; do
-        $benchmark_cmd | grep "requests per second" >>temp.txt
-    done
-) 
-end_time=$(date +%s)                # 记录结束时间
-duration=$((end_time - start_time)) # 计算持续时间
-echo "Total duration: $duration seconds" >>temp.txt
+# CPU 使用率
+monitor_duration="4"
+mpstat -P ALL 1 $monitor_duration >temp_cpu_usage.txt &
+
+# 内存使用率
+vmstat 1 $monitor_duration >temp_mem_usage.txt &
+
+#  磁盘利用率
+iostat 1 $monitor_duration >temp_disk_usage.txt &
+
+# script -c "timeout 4 iftop -t" temp_iftop_output.txt
