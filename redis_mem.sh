3142ac30a6c302effa6cb8c5771a942d20b8da8d
tjujingzong <470699397@qq.com>
2023-12-21 16:08:43 +0800

    record again

--- redis_mem.sh
+++ redis_mem.sh
@@ -1,42 +1,50 @@
 #!/bin/bash
 
-memory_levels=("4K")
-for ((mem = 256; mem <= 7680; mem += 512)); do
+memory_levels=()
+for ((mem = 6656; mem >= 0; mem -= 512)); do
     memory_levels+=("${mem}M")
 done
+memory_levels+=("4k")
+# cpu_levels=(100)
+# io_levels=(100)
+cpu_levels=(90 70 50 30 10 0)
+io_levels=(50 40 30 20 10 1)
 
-cpu_levels=(0 1 2 3 4)
-io_levels=(0 1 2 3 4)
-
-benchmark_duration="15"
+benchmark_duration="200"
 monitor_duration="6" # 设置监控时长短于压力测试时长
 dir="results_redis"
 mkdir -p $dir
 current_datetime=$(date +%Y%m%d%H%M)
 
 # File paths
-sys_result="$dir/redis_sys_$current_datetime.txt"
-latency="$dir/redis_lan_$current_datetime.txt"
-vmstate="$dir/redis_vmstate_$current_datetime.txt"
+sys_result="$dir/$current_datetime-redis_sys.txt"
+latency="$dir/$current_datetime-redis_lan.txt"
+vmstate="$dir/$current_datetime-redis_vmstate.txt"
 
 echo "$USER ALL=(ALL) NOPASSWD: /usr/bin/perf stat" | sudo tee /etc/sudoers.d/perf_stat_nopass
 monitor_performance() {
-    sleep 6
-    (
-        echo "start monitoring......"
-        docker exec -i "redis-server" timeout $monitor_duration redis-cli --latency >"redis_latency_results.txt"
-    ) &
+    sleep 3
+    docker exec -i "redis-server" timeout $monitor_duration redis-cli --latency >"redis_latency_results.txt" &
+    local pid_redis=$!
 
     sudo perf stat -d -x, -a -o temp2.txt sleep $monitor_duration &
+    local pid_perf=$!
+
     vmstat 1 $monitor_duration >temp_mem_usage.txt &
+    local pid_vmstat=$!
+
+    # 等待特定的后台进程
+    wait $pid_redis
+    wait $pid_perf
+    wait $pid_vmstat
 
     # Cleanup
-    wait
+    echo "end monitor"
     pkill stress-ng
     cat temp2.txt >>$sys_result
     cat "redis_latency_results.txt" >>$latency
     cat temp_mem_usage.txt >>$vmstate
-    sleep 2
+    sleep 1
 }
 
 # Starting experiments
@@ -46,8 +54,11 @@ for io in ${io_levels[@]}; do
     for cpu in ${cpu_levels[@]}; do
         for mem in ${memory_levels[@]}; do
             # Apply stress in background
-            stress-ng --cpu $cpu --vm 2 --vm-bytes $mem --io $io --timeout $benchmark_duration &
-            monitor_performance
+            printf "$mem $cpu $io " >>$latency
+            sleep 2
+            stress-ng --cpu 4 --cpu-load $cpu --vm 2 --vm-bytes $mem --iomix 2 --iomix-bytes $io% --timeout $benchmark_duration &
+            monitor_performance &
+            wait
         done
     done
 done
